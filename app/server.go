2f0441ab693bfb7e709f856c7a9586bc3a1541ff
harish876 <harishgokul01@gmail.com>
2024-04-03 16:34:24 +0530

    200 response

--- app/server.go
+++ app/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
 )
@@ -9,15 +11,38 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	for {
+		buf := make([]byte, 1024)
+		recievedBytes, err := conn.Read(buf)
+		if err == io.EOF || err != nil {
+			log.Println(err)
+			break
+		}
+		request := buf[:recievedBytes]
+		log.Println(request)
+
+		successResponse := "HTTP/1.1 200 OK\r\n\r\n"
+		sentBytes, err := conn.Write([]byte(successResponse))
+		if err != nil {
+			log.Println("Error writing response: ", err.Error())
+		}
+		log.Println("Sent Bytes to Client: ", sentBytes)
 	}
 }
